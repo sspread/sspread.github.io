78dd312fc446972b5c339df6d13c43c36538e949
sspread <sspread2@gmail.com>
2014-07-24 13:01:23 -0500

    Add collapse articles and new color scheme

---
+++ scripts/posttoggle.js
@@ -0,0 +1,29 @@
+$("#post_link6").click(function() {
+	event.preventDefault();
+  $( ".post.6" ).slideToggle( "slow" );
+});
+
+$("#post_link5").click(function() {
+	event.preventDefault();
+  $( ".post.5" ).slideToggle( "slow" );
+});
+
+$("#post_link4").click(function() {
+	event.preventDefault();
+  $( ".post.4" ).slideToggle( "slow" );
+});
+
+$("#post_link3").click(function() {
+	event.preventDefault();
+  $( ".post.3" ).slideToggle( "slow" );
+});
+
+$("#post_link2").click(function() {
+	event.preventDefault();
+  $( ".post.2" ).slideToggle( "slow" );
+});
+
+$("#post_link1").click(function() {
+	event.preventDefault();
+  $( ".post.1" ).slideToggle( "slow" );
+});
