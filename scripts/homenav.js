2131dacc9546913a8796dc75893ae2e0ba65d9e3
sspread <sspread2@gmail.com>
2014-08-22 16:25:12 -0500

    Improve nav bar for mabile

--- scripts/homenav.js
+++ scripts/homenav.js
@@ -1,4 +1,51 @@
-    $('#home-nav-wrap').height($('#home-nav').height());
-    $('#home-nav').affix({
-        offset: $('#home-nav').position()
-    });
+
+$('#home-nav-wrap').height($('#home-nav').height());
+$('#home-nav').affix({
+    offset: $('#home-nav').position()
+});
+
+// $(document).ready(function() {
+//     var width = $(window).width();
+//     if (width <= 768) {
+    	
+
+//         } else {
+//         $('#home-nav.affix-top #nav-row').collapse('show');
+//         $('#home-nav.affix #nav-row').collapse('show');
+
+
+//     };
+// })
+
+$(document).ready(function() {
+
+$(window).resize(function() {
+    var width = $(window).width();
+    if (width <= 768) {
+    	
+        $('#home-nav.affix-top #nav-row').collapse('show');
+        $('#home-nav.affix #nav-row').collapse('hide');
+
+        } else {
+        $('#home-nav.affix-top #nav-row').collapse('show');
+        $('#home-nav.affix #nav-row').collapse('show');
+
+
+    };
+})
+
+$(window).scroll(function() {
+    var width = $(window).width();
+    if (width <= 768) {
+    	
+        $('#home-nav.affix-top #nav-row').collapse('show');
+        $('#home-nav.affix #nav-row').collapse('hide');
+
+        } else {
+        $('#home-nav.affix-top #nav-row').collapse('show');
+        $('#home-nav.affix #nav-row').collapse('show');
+
+
+    };
+})
+});
