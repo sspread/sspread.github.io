08723ad073dc12dab779e4e54a078e71f808eb46
sspread <sspread2@gmail.com>
2014-08-22 16:35:17 -0500

    Improve nav bar for mabile

--- scripts/homenav.js
+++ scripts/homenav.js
@@ -4,48 +4,10 @@ $('#home-nav').affix({
     offset: $('#home-nav').position()
 });
 
-// $(document).ready(function() {
-//     var width = $(window).width();
-//     if (width <= 768) {
-    	
-
-//         } else {
-//         $('#home-nav.affix-top #nav-row').collapse('show');
-//         $('#home-nav.affix #nav-row').collapse('show');
-
-
-//     };
-// })
-
-$(document).ready(function() {
-
-$(window).resize(function() {
+$(window).bind("resize scroll", function() {
     var width = $(window).width();
     if (width <= 768) {
-    	
         $('#home-nav.affix-top #nav-row').collapse('show');
         $('#home-nav.affix #nav-row').collapse('hide');
-
-        } else {
-        $('#home-nav.affix-top #nav-row').collapse('show');
-        $('#home-nav.affix #nav-row').collapse('show');
-
-
     };
-})
-
-$(window).scroll(function() {
-    var width = $(window).width();
-    if (width <= 768) {
-    	
-        $('#home-nav.affix-top #nav-row').collapse('show');
-        $('#home-nav.affix #nav-row').collapse('hide');
-
-        } else {
-        $('#home-nav.affix-top #nav-row').collapse('show');
-        $('#home-nav.affix #nav-row').collapse('show');
-
-
-    };
-})
 });
